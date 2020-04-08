d4f4217b922d4dcf70a9fb9bf020e0ec7c2eb0e2
dtr <tarasi44444@gmail.com>
2020-04-08 19:04:50 +0300

    Add the steps to the methods

--- src/test/java/pages/blocks/common/HeaderBlock.java
+++ src/test/java/pages/blocks/common/HeaderBlock.java
@@ -1,8 +1,8 @@
-package pages.blocks;
+package pages.blocks.common;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import pages.blocks.common.BaseBlock;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -10,11 +10,13 @@ public class HeaderBlock extends BaseBlock {
   private SelenideElement searchField = $(By.id("query"));
   private SelenideElement searchButton = $(By.xpath("//input[@value = 'Search']"));
 
+  @Step("Enter data in the 'search field")
   public HeaderBlock enterDataInTheSearchField(String data) {
     searchField.sendKeys(data);
     return this;
   }
 
+  @Step("Click the 'Search' button")
   public HeaderBlock clickSearchButton() {
     searchButton.click();
     return this;
