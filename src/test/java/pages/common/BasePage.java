a4d920207a4491d2e8dffbd2da5dbb09f5106c8e
dtr <tarasi44444@gmail.com>
2020-04-08 19:03:48 +0300

    Add the 'Header' block

--- src/test/java/pages/common/BasePage.java
+++ src/test/java/pages/common/BasePage.java
@@ -1,11 +1,14 @@
 package pages.common;
 
+import pages.blocks.common.HeaderBlock;
 import utils.DriverHelper;
 
 public abstract class BasePage {
+  public HeaderBlock headerBlock;
   public DriverHelper h;
 
   public BasePage() {
     h = new DriverHelper();
+    headerBlock = new HeaderBlock();
   }
 }
