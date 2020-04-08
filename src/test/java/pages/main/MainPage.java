3c52f0728091b5b251ef3c50b22adca789620490
dtr <tarasi44444@gmail.com>
2020-04-08 19:05:44 +0300

    Delete the 'Header Block' object

--- src/test/java/pages/main/MainPage.java
+++ src/test/java/pages/main/MainPage.java
@@ -1,12 +1,7 @@
 package pages.main;
 
-import pages.blocks.HeaderBlock;
 import pages.common.BasePage;
 
 public class MainPage extends BasePage {
-  public HeaderBlock headerBlock;
 
-  public MainPage() {
-    headerBlock = new HeaderBlock();
-  }
 }
