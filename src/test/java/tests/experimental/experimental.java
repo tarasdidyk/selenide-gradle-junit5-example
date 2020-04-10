ce18e60c8e3d100880df1ccdec1cc34607acf290
dtr <tarasi44444@gmail.com>
2020-04-10 19:50:05 +0300

    Edit experimental

--- src/test/java/tests/experimental/experimental.java
+++ src/test/java/tests/experimental/experimental.java
@@ -1,24 +1,27 @@
 package tests.experimental;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.main.MainPage;
 import pages.search.SearchResultPage;
 import tests.common.BaseTest;
-import utils.enums.RepositoryNames;
+import utils.enums.RepositoryGroup;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class experimental extends BaseTest {
 
-  @Tag("test")
+  @Tag("search")
+  @DisplayName("Is the 0.2.0 version of the Github TrieMap collection displayed")
   @Test
-  public void test() {
+  public void isTheLastVersionOfTheGitHubTrieMapCollectionDisplayed() {
     MainPage mainPage = new MainPage();
     SearchResultPage searchResultPage = new SearchResultPage();
     open("");
-    mainPage.headerBlock.enterDataInTheSearchField("java");
+    mainPage.headerBlock.enterDataInTheSearchField("TrieMap");
     mainPage.headerBlock.clickSearchButton();
-    searchResultPage.leftSearchResultMenuFiltersBlock.selectRepositoryFilter(RepositoryNames.CENTRAL);
+    searchResultPage.leftSearchResultMenuFiltersBlock.filterByGroup(RepositoryGroup.GITHUB);
+    searchResultPage.searchResultBlock.isRepositoryDisplayed("dw", "dw");
   }
 }
