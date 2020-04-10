2be709cf596e1f13c867b08fb83d4f3a50c1c098
dtr <tarasi44444@gmail.com>
2020-04-10 19:51:08 +0300

    Refactor the 'Select repository filter' step

--- src/test/java/pages/blocks/searchresult/LeftSearchResultMenuFiltersBlock.java
+++ src/test/java/pages/blocks/searchresult/LeftSearchResultMenuFiltersBlock.java
@@ -4,17 +4,19 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import pages.blocks.common.BaseBlock;
-import utils.enums.RepositoryNames;
+import utils.enums.RepositoryGroup;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LeftSearchResultMenuFiltersBlock extends BaseBlock {
-  SelenideElement filterMenuContainer = $(By.xpath("//div[@id = 'left']"));
+  private SelenideElement filterMenuContainer = $(By.xpath("//div[@id = 'left']"));
+
+  private SelenideElement groupContainer = filterMenuContainer.$(By.xpath(".//h3[text() = 'Group']/.."));
 
   @Step("select repository filter")
-  public LeftSearchResultMenuFiltersBlock selectRepositoryFilter(RepositoryNames repositoryName) {
-    String locator = String.format(".//span[text() = '%s']", repositoryName.toString());
-    SelenideElement selectItem = filterMenuContainer.$(By.xpath(locator));
+  public LeftSearchResultMenuFiltersBlock filterByGroup(RepositoryGroup repositoryGroupName) {
+    String locator = String.format(".//span[text() = '%s']", repositoryGroupName.toString());
+    SelenideElement selectItem = groupContainer.$(By.xpath(locator));
     selectItem.click();
     return this;
   }
