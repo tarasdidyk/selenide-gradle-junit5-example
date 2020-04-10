f9e8f42ecfe46e0770f84edd3a0402b66650f86c
dtr <tarasi44444@gmail.com>
2020-04-10 19:50:30 +0300

    Add the 'Search Result' object

--- src/test/java/pages/search/SearchResultPage.java
+++ src/test/java/pages/search/SearchResultPage.java
@@ -1,12 +1,15 @@
 package pages.search;
 
 import pages.blocks.searchresult.LeftSearchResultMenuFiltersBlock;
+import pages.blocks.searchresult.SearchResultBlock;
 import pages.common.BasePage;
 
 public class SearchResultPage extends BasePage {
   public LeftSearchResultMenuFiltersBlock leftSearchResultMenuFiltersBlock;
+  public SearchResultBlock searchResultBlock;
 
   public SearchResultPage() {
     leftSearchResultMenuFiltersBlock = new LeftSearchResultMenuFiltersBlock();
+    searchResultBlock = new SearchResultBlock();
   }
 }
