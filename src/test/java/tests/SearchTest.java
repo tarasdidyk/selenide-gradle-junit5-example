df3d8c220fa5d0d8db3a609f3be48eb507bc4723
dtr <tarasi44444@gmail.com>
2020-04-19 15:59:29 +0300

    Add the 'Is the 'Github TrieMap' repository displayed after searching' test

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,7 +1,27 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.main.MainPage;
+import pages.search.SearchResultPage;
 import tests.common.BaseTest;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTest extends BaseTest {
 
+  @Tag("search")
+  @DisplayName("Is the 'Github TrieMap' repository displayed after searching")
+  @Test
+  public void isTheGithubTrieMapRepositoryDisplayedAfterSearching() {
+    MainPage mainPage = new MainPage();
+    SearchResultPage searchResultPage = new SearchResultPage();
+    open("");
+    mainPage.headerBlock.enterDataInTheSearchField("TrieMap");
+    mainPage.headerBlock.clickSearchButton();
+    Assertions.assertTrue(searchResultPage.searchResultBlock.isRepositoryDisplayed("TrieMap", "com.github")
+        , "The repository is not displayed");
+  }
 }
